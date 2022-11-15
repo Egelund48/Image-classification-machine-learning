675fe5dad568096ad62ec087b4d4409512a11464
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2022-11-15 21:44:59 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np 
 
-def model_arch(): 
+def model_arch():
     models = tf.keras.models.Sequential()
-
-    models.add(tf.keras.layers.Conv2D(64, (5,5), padding="same", activation="relu", input_shape=(28,28,1)))
-
-    models.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
-    models.add(tf.keras.layers.Conv2D(256,(5,5), padding="same", activation="relu"))
-
-    models.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
-
+     
+    models.add(tf.keras.layers.Conv2D(64, (5, 5), padding="same", activation="relu", input_shape=(28, 28, 1)))
+     
+    models.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    models.add(tf.keras.layers.Conv2D(128, (5, 5), padding="same", activation="relu"))
+     
+    models.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    models.add(tf.keras.layers.Conv2D(256, (5, 5), padding="same", activation="relu"))
+     
+    models.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+     
     models.add(tf.keras.layers.Flatten())
-    models.add(tf.keras.layers.Dense(256,activation="relu"))
-
+    models.add(tf.keras.layers.Dense(256, activation="relu"))
+     
     models.add(tf.keras.layers.Dense(10, activation="softmax"))
-
-    return models 
+    return models
