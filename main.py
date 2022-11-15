c42260198bca93aaca22d98239a3e60f3e9226bb
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2022-11-15 21:44:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -14,13 +14,33 @@ plt.imshow(trainX[0], cmap=plt.get_cmap('gray'))
 plt.show()
 ''' 
 
-trainX = np.expand_dims(trainX, 1)
-testX = np.expand_dims(testX, 1)
+trainX = np.expand_dims(trainX, -1)
+testX = np.expand_dims(testX, -1)
 
 model = model_arch()
 
-model.compile(tf.keras.optimizers.Adam(learning_rate=1e-3), loss="sparse_categorical_crossentropy", metrics=['sparse_categorical_accuracy'])
+model.compile(tf.keras.optimizers.Adam(learning_rate=1e-3), loss="sparse_categorical_crossentropy", metrics=["sparse_categorical_accuracy"])
 
-Traning_historie = model.fit(trainX.astype(np.float32), trainY.astype(np.float32), epochs=10, steps_per_epoch=100, validation_split=0.33)
+#model.summary()
+
+#Traning_historie=model.fit(trainX.astype(np.float32), trainY.astype(np.float32), epochs=20, steps_per_epoch=100, validation_steps=0.33)
 
 model.save_weights("./model.h5", overwrite=True)
+
+'''
+plt.plot(Traning_historie.history['sparse_categorical_accuracy'])
+plt.plot(Traning_historie.history['val_sparse_categorical_accuracy'])
+plt.title('Modellens nøjagtighed')
+plt.ylabel('Nøjagtighed')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
+'''
+labels = ["T-shirt", "Bukser", "Pullover", "Kjole", "Frakke", "Sandal", "Skjorte", "Sneaker", "Taske", "Ankel sko"]
+
+predictions = model.predict(testX[:1])
+label = labels[np.argmax(predictions)]
+ 
+print(label)
+plt.imshow(testX[:1][0])
+plt.show()
